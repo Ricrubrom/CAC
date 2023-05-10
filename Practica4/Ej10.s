d628c6c5b2f136e6b0a9617021d67b4808b2501b
Mateo Romero <materomero04@gmail.com>
2023-05-10 17:02:24 -0300

    arreglado

--- Practica4/Ej10.s
+++ Practica4/Ej10.s
@@ -4,7 +4,7 @@ car:    .asciiz "d" ; caracter buscado
 cant:   .word   0 ; cantidad de veces que se repite el caracter car en cadena.
 
 .code
-        lbu      r1, car(r0)
+        lbu     r1, car(r0)
         dsub    r3, r3, r3
         dsub    r4, r4, r4
 loop:   lbu     r2, cadena(r3)
@@ -12,7 +12,7 @@ loop:   lbu     r2, cadena(r3)
         dsub    r5, r2, r1
         bnez    r5, sigo
         daddi   r4, r4, 1
-sigo:   daddi r3, r3, 8
+sigo:   daddi   r3, r3, 1
         beqz    r0, loop
-fin:    sb r4, cant(r0)
+fin:    sb      r4, cant(r0)
         halt
